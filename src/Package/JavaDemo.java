67d96cbad88cae1e45d5b274edc65fca7644a5b5
yif <csuheshibo@163.com>
2019-05-29 15:13:27 +0800

    Package

---
+++ src/Package/JavaDemo.java
@@ -0,0 +1,11 @@
+package Package;
+
+import Test.Person;
+
+public class JavaDemo {
+    public static void main(String[] args) {
+        Person p = new Person("heshibo",29);
+        p.tell();
+
+    }
+}
